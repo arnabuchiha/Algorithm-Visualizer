d27c2dbdc04b3d6ace266573e7c008a87af85bdb
arnabuchiha <arnabkeya04@gmail.com>
2020-09-15 23:22:18 +0530

    Front page done

--- src/Home.js
+++ src/Home.js
@@ -24,13 +24,13 @@ class Home extends Component{
     render(){
         return(
             <div >
-                <img className="img-fluid mt-4 m-4" src={Logo}/>
+                <img className="img-fluid mt-4" src={Logo}/>
                 <ul class="list-inline mt-5">
                     {this.state.problems.map(element=>
-                        <li className="list-inline-item" style={{cursor:"pointer"}} onClick={()=>window.location.href=element.link}>
+                        <li className="list-inline-item mb-2" style={{cursor:"pointer"}} onClick={()=>window.location.href=element.link}>
                         <div className="card card1" style={{width: "14rem"}}>
                             <img className="card-img-top img-thumbnail algo-image" src={element.imgUrl} alt="Card image cap"/>
-                            <div style={{overflow:"hidden"}} className="card-body">
+                            <div style={{overflow:"hidden",backgroundColor:"#5bc9b1"}} className="card-body">
                                 <h3 className="card-text algo-name">{element.name}</h3>
                             </div>
                         </div>
