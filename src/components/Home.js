a19a588b18af38bff2a07341f9ed0cd09e131d2f
arnabuchiha <arnabkeya04@gmail.com>
2021-04-26 00:47:18 +0530

    Final Home UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import "../styles/Home.css"
 import Logo from "../assets/heading_logo.png";
 import convexhull from "../assets/convex_hull.png";
-import sorting from "../assets/sorting.png";
+import sorting from "../assets/sorting.png"
+import "particles.js";
+import particleJSON from "../assets/particles.json";
 import pathfinding_front from "../assets/pathfinding_front.svg";
 import {ReactComponent as Github} from "../assets/github.svg";
 import {ReactComponent as LinkedIn} from "../assets/linkedin.svg";
 import {ReactComponent as LogoIcon} from "../assets/analytics.svg";
+import bar from "../assets/bar-chart.svg";
+import graph from "../assets/graph.svg";
+import convex from "../assets/convex.svg";
 class Home extends Component{
     constructor(){
         super();
@@ -14,18 +19,18 @@ class Home extends Component{
             problems:[
                 {
                     name:"Sorting",
-                    imgUrl:sorting,
+                    imgUrl:bar,
                     link:"/sorting"
                     
                 },
                 {
                     name:"Pathfinding",
-                    imgUrl:pathfinding_front,
+                    imgUrl:graph,
                     link:"/pathfind"
                 },
                 {
                     name:"ConvexHull",
-                    imgUrl:convexhull,
+                    imgUrl:convex,
                     link:"/convexhull"
                 }
             ]
@@ -33,48 +38,56 @@ class Home extends Component{
     }
     componentDidMount(){
         
-    //   window.particlesJS("particles-js",particleJSON );
+      window.particlesJS("particles-js",particleJSON );
       
     }
     render(){
         return(
-            <div class="container">
-                <div class="box title">
+            <div className="back">
+                <div id="particles-js"></div>
+              <div class="box title">
                     <div className="container-fluid solid">
                         <ul className="links">
-                            <li>
+                            <li onClick={()=>window.open("https://github.com/arnabuchiha/Algorithm-Visualizer","_blank")}>
                                 <i>
                                     <Github/>
                                 </i>
                             </li>
-                            <li>
+                            <li onClick={()=>window.open("https://www.linkedin.com/in/funky-poseidon/","_blank")}>
                                 <i>
                                     <LinkedIn/>
                                 </i>
                             </li>
                         </ul>
-                        <h1 className="main-title center">
+                        
+                    </div>
+                </div>
+                    
+                <h1 className="main-title center">
                             <div className="logo">
                             <LogoIcon/>
                             </div>
-                            Algorithm <strong style={{color:"#5BC9B1"}}>Visualizer</strong>
-                        </h1>
-                    </div>
+                            ALGORITHM <strong style={{color:"var(--home-color)"}}><br></br>VISUALIZER</strong>
+                </h1>
+                <div>
+                    <p className="description-title center">
+                    Algorithm Visualizer is an interactive online platform that visualizes algorithms from code.
+                    Currently these include Sorting, Pathfind and ConvexHull Algorithms.
+                    More Algorithms will be coming soon!!
+                    </p>
                 </div>
-                <div class="box cards stack-top">
-                <ul className="list-inline mt-5" style={{display:"block",position:"relative"}}>
-                     {this.state.problems.map((element,i)=>
-                        <li key={i} className="list-inline-item mb-2" style={{cursor:"pointer"}} onClick={()=>window.location.href=element.link}>
-                        <div className="card card1" style={{width: "14rem"}}>
-                            <img className="card-img-top img-thumbnail algo-image" src={element.imgUrl} alt="Card image cap"/>
-                            <div style={{overflow:"hidden"}} className="card-body">
+                <ul class="list-inline mt-5" style={{display:"block",position:"relative"}}>
+                    {this.state.problems.map(element=>
+                        <li className="list-inline-item ml-5 mr-5 mb-2" style={{cursor:"pointer"}} onClick={()=>window.location.href=element.link}>
+                        <div className="card" style={{width: "14rem"}}>
+                            <img className="card-img-top img algo-image" src={element.imgUrl} alt="Card image cap"/>
+                            <div style ={{backgroundColor:"transparent"}} className="card-body">
                                 <h3 className="card-text algo-name">{element.name}</h3>
                             </div>
                         </div>
                         </li>
                     )}
                 </ul>
-                </div>
             </div>
         )
     }
